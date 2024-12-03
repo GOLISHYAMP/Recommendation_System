101f5893d19e4fe30ae690af404b7f6979e71d87
Shyam Goli <golishyamgolisai@gmail.com>
2024-12-04 01:10:24 +0530

    almost streamlit app is done

--- app.py
+++ app.py
@@ -5,132 +5,184 @@ from PIL import Image
 import os
 from io import BytesIO
 
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer=WordNetLemmatizer()
+from nltk import sent_tokenize, word_tokenize
+import numpy as np
+import pandas as pd
+
+base_path = os.getcwd()
+sub_path = 'data'
+dataset_path = os.path.join(base_path, sub_path, 'mobile_recommendation_system_dataset.csv')
+
+# import df
+df = pd.read_csv(dataset_path,index_col=False)
+def recommend(mobile):
+    mobile_index = df[df['name']==mobile].index[0]
+    similarity_array = similarity[mobile_index]
+    similar_10_mobiles = sorted(list(enumerate(similarity_array)),reverse=True,key=lambda x:x[1])[1:11]
+    return similar_10_mobiles
+
+
+def avg_word2vec(doc):
+    return np.mean([name_model.wv[word] for word in doc if word in name_model.wv.index_to_key],axis=0)
+
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="PickYourPhone", page_icon=":mobile_phone:", layout="wide")
 
 
-# def load_lottieurl(url):
-#     r = requests.get(url, verify=False)
-#     if r.status_code != 200:
-#         return None
-#     return r.json()
 
-# def load_lottieurl(url):
-#     import requests
+# def load_lottiefile(filepath):
+#     import json
 #     try:
-#         r = requests.get(url, verify=False)
-#         print(f"Response Status Code: {r.status_code}")
-#         print(f"Response Content: {r.text}")  # Print the content for debugging
-#         r.raise_for_status()
-#         return r.json()  # Try to parse JSON
-#     except requests.exceptions.RequestException as e:
-#         print(f"Request error: {e}")
-#         return None
-#     except ValueError as e:
-#         print("JSON decoding failed:", e)
+#         with open(filepath, "r", encoding="utf-8") as file:
+#             return json.load(file)
+#     except Exception as e:
+#         print(f"Error loading file: {e}")
 #         return None
 
-
-def load_lottiefile(filepath):
-    import json
-    try:
-        with open(filepath, "r", encoding="utf-8") as file:
-            return json.load(file)
-    except Exception as e:
-        print(f"Error loading file: {e}")
-        return None
-
-lottie_coding = load_lottiefile(os.path.join(os.getcwd(), "Animation - 1733047553544.json"))
+# lottie_coding = load_lottiefile(os.path.join(os.getcwd(), "Animation - 1733047553544.json"))
 
 
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# # Use local CSS
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("style/style.css")
+# local_css("style/style.css")
 
-# ---- LOAD ASSETS ----
-# lottie_coding = load_lottieurl("https://lottie.host/b5066c54-82de-4c32-93e0-046f9c4a17c9/gUv6XtilSp.lottie")
-# img_contact_form = Image.open("images/yt_contact_form.png")
-# img_lottie_animation = Image.open("images/yt_lottie_animation.png")
-img_contact_form = load_lottiefile(os.path.join(os.getcwd(), "Animation - 1733047553544.json"))
-img_lottie_animation = load_lottiefile(os.path.join(os.getcwd(), "Animation - 1733047553544.json"))
-# ---- HEADER SECTION ----
-with st.container():
-    st.subheader("Hi, I am Sven :wave:")
-    st.title("A Data Analyst From Germany")
-    st.write(
-        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
-    )
-    st.write("[Learn More >](https://pythonandvba.com)")
+# # ---- LOAD ASSETS ----
+# # lottie_coding = load_lottieurl("https://lottie.host/b5066c54-82de-4c32-93e0-046f9c4a17c9/gUv6XtilSp.lottie")
+# # img_contact_form = Image.open("images/yt_contact_form.png")
+# # img_lottie_animation = Image.open("images/yt_lottie_animation.png")
+# img_contact_form = load_lottiefile(os.path.join(os.getcwd(), "Animation - 1733047553544.json"))
+# img_lottie_animation = load_lottiefile(os.path.join(os.getcwd(), "Animation - 1733047553544.json"))
+# # ---- HEADER SECTION ----
+# with st.container():
+#     st.subheader("Hi, I am Sven :wave:")
+#     st.title("A Data Analyst From Germany")
+#     st.write(
+#         "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+#     )
+#     st.write("[Learn More >](https://pythonandvba.com)")
  
-# ---- WHAT this project does explaination ----
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("What I do")
-        st.write("##")
-        st.write(
-            """
-            On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way."
+# # ---- WHAT this project does explaination ----
+# with st.container():
+#     st.write("---")
+#     left_column, right_column = st.columns(2)
+#     with left_column:
+#         st.header("What I do")
+#         st.write("##")
+#         st.write(
+#             """
+#             On my YouTube channel I am creating tutorials for people who:
+#             - are looking for a way to leverage the power of Python in their day-to-day work.
+#             - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+#             - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+#             - are working with Excel and found themselves thinking - "there has to be a better way."
 
-            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
-            """
-        )
-        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
-    with right_column:
-        st_lottie(lottie_coding, height=300, key="coding")
+#             If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+#             """
+#         )
+#         st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
+#     with right_column:
+#         st_lottie(lottie_coding, height=300, key="coding")
 
 # ---- Recommendations ----
-with st.container():
-    st.write("---")
-    st.header("My Projects")
-    st.write("##")
-    image_column, text_column = st.columns((1, 2))
-    with image_column:
-        # URL of the image
-        image_url = "https://rukminim2.flixcart.com/image/312/312/l4ln8nk0/mobile/6/s/i/-original-imagfgf9nf8psuyv.jpeg?q=70"
+import pickle
+# Load the saved model
+with open(r'src\model\similarity.pkl', 'rb') as file:
+    similarity = pickle.load(file)
 
-        # Fetch and display the image
-        st.title("Display Image from URL")
-        # image = fetch_image_with_advanced_headers(image_url)
+with open(r'src\model\name_model.pkl', 'rb') as file:
+    name_model = pickle.load(file)
 
-        # if image:
-        #     st.image(image, caption="Fetched Image", use_column_width=True)
-        # else:
-        #     st.error("Failed to load image.")
-    with text_column:
-        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
-        st.write(
-            """
-            Learn how to use Lottie Files in Streamlit!
-            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
-            In this tutorial, I'll show you exactly how to do it
-            """
-        )
-        st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
-with st.container():
-    image_column, text_column = st.columns((1, 2))
-    with image_column:
-        # st_lottie(img_contact_form)
-        pass
-    with text_column:
-        st.subheader("How To Add A Contact Form To Your Streamlit App")
-        st.write(
-            """
-            Want to add a contact form to your Streamlit website?
-            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
-            """
-        )
-        st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
+with open(r'src\model\name_vectors.pkl', 'rb') as file:
+    name_vectors = pickle.load(file)
 
+
+
+
+st.write("---")
+st.header("Mobile Recommendation")
+st.write("##")
+
+# Set the title
+st.title("Search for your mobile")
+
+# Add a text input with a search icon as a placeholder
+input_string = st.text_input("Search", placeholder="🔍 Type your query here...")
+
+similar_10_mobiles = None
+# Function to handle card selection
+def handle_card_selection(card_text):
+    global similar_10_mobiles, input_string
+    st.session_state.selected_card = card_text
+    print(card_text)
+    similar_10_mobiles = recommend(card_text)
+    st.rerun()
+
+# Initialize session state
+if "input_string" not in st.session_state:
+    st.session_state.input_string = ""
+if "selected_card" not in st.session_state:
+    st.session_state.selected_card = None
+
+
+# Display the entered search query
+if input_string:
+    if st.session_state.selected_card:
+        input_string = st.session_state.selected_card
+    st.session_state.input_string = input_string
+    st.write(f"You searched for: {st.session_state.input_string}")
+
+    input_string = st.session_state.input_string.lower()
+    input_string = input_string.split()
+    input_string = [lemmatizer.lemmatize(word) for word in input_string]
+    input_string = ' '.join(input_string)
+    # print(input_string)
+    sent_token=sent_tokenize(input_string)
+    # print(sent_token)
+    sim_pro = word_tokenize(sent_token[0])
+    # print(sim_pro)
+    w2v = avg_word2vec(sim_pro)
+    # print(w2v)
+
+    w2v_reshaped = w2v.reshape(1,-1)
+    from sklearn.metrics.pairwise import cosine_similarity
+    similarity_name_words = cosine_similarity(w2v_reshaped, name_vectors)
+
+    similar_10_mobiles = sorted(list(enumerate(similarity_name_words[0])),reverse=True,key=lambda x:x[1])[0:10]
+
+    with st.container():
+        for i in similar_10_mobiles:
+            title_column, text_column = st.columns((1, 2))
+            with title_column:
+                # st.title()
+                if st.button(df['name'].iloc[i[0]]):
+                    handle_card_selection(df['name'].iloc[i[0]])
+            with text_column:
+                st.subheader(df['ratings'].iloc[i[0]], df['price'].iloc[i[0]])
+                st.write(
+                    df['corpus'].iloc[i[0]]
+                )
+          
+if st.session_state.selected_card:
+    similar_10_mobiles = recommend(st.session_state.selected_card)
+    with st.container():
+        for i in similar_10_mobiles:
+            title_column, text_column = st.columns((1, 2))
+            with title_column:
+                # st.title()
+                if st.button(df['name'].iloc[i[0]]):
+                    handle_card_selection(df['name'].iloc[i[0]])
+            with text_column:
+                st.subheader(df['ratings'].iloc[i[0]], df['price'].iloc[i[0]])
+                st.write(
+                    df['corpus'].iloc[i[0]]
+                )
 # ---- CONTACT ----
 # with st.container():
 #     st.write("---")
@@ -152,3 +204,18 @@ with st.container():
 #         st.markdown(contact_form, unsafe_allow_html=True)
 #     with right_column:
 #         st.empty()
+
+###############################################################
+# st.write("Most objects") # df, err, func, keras!
+# # st.write_stream(my_generator)
+# # st.write_stream(my_llm_stream)
+
+# st.text("Fixed width text")
+# st.markdown("_Markdown_")
+# st.title("My title")
+# st.header("My header")
+# st.subheader("My sub")
+
+# st.json({"foo":"bar","fu":"ba"})
+# st.metric("My metric", 42, 2)
+#####################################################################
